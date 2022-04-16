7b1fc1196eb30e5522f98d088836aedd49302cbf
yuboshao <yuboshaouoe@gmail.com>
2022-04-16 21:48:26 +0100

    logging finished by Callum

--- src/main/java/logging/LogEntry.java
+++ src/main/java/logging/LogEntry.java
@@ -4,13 +4,24 @@ import java.util.Map;
 
 public class LogEntry {
 
+    private String callerName;
+    private Object result;
+    private Map<String,Object> additionalInfo;
+
     LogEntry(String callerName,
-              Object result,
-              Map<String,Object> additionalInfo){}
+             Object result,
+             Map<String,Object> additionalInfo){
+        this.callerName = callerName;
+        this.result = result;
+        this.additionalInfo = additionalInfo;
+    }
 
-    public String getResult(){}
+    public Object getResult(){
+        return result.toString();
+    }
 
-    public String toString(){}
-
-
+    public String toString(){
+        String output = "Caller Name: " + this.callerName + "\nResult: " + this.result;
+        return output;
+    }
 }
--- src/main/java/logging/Logger.java
+++ src/main/java/logging/Logger.java
@@ -1,24 +1,47 @@
 package logging;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.List;
 
 public class Logger{
 
-    public static Logger getInstance(){}
+    private static List<Logger> allLog = null;
+    private static List<LogEntry> fullLog = null;
+
+    private Logger(){}
+
+    public static Logger getInstance(){
+        Logger log;
+        if(allLog.isEmpty()){       //ONLY CREATE LOGGER IF ONE DOESN'T EXIST, OTHERWISE RETRIEVE EXISTING
+            log = new Logger();
+            allLog.add(log);
+        }
+        else{
+            log = allLog.get(0);
+        }
+        return log;
+    }
 
     public void logAction(String callerName,
-                           Object result){
+                          Object result){
         LogEntry newEntry = new LogEntry(callerName, result, Collections.emptyMap());
         this.fullLog.add(newEntry);
     }
 
     public void logAction(String callerName,
-                           Object result,
-                           Map<String,Object> additionalInfo){}
+                          Object result,
+                          Map<String,Object> additionalInfo){
+        LogEntry newEntry = new LogEntry(callerName, result, additionalInfo);
+        this.fullLog.add(newEntry);
+    }
 
-    public List<LogEntry> getLog(){}
+    public List<LogEntry> getLog(){
+        return this.fullLog;
+    }
 
-    public void clearLog(){}
+    public void clearLog(){
+        this.fullLog.clear();
+    }
 
 }
