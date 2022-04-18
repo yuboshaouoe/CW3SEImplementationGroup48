0b50b51b7d19d9a5a73bc9cef23d1b401e2bbf20
yuboshao <yuboshaouoe@gmail.com>
2022-04-18 11:17:04 +0100

    Changes by jiajili

---
+++ src/test/java/command/EventSystemTest.java
@@ -0,0 +1,31 @@
+package command;
+import command.*;
+import controller.Controller;
+import logging.Logger;
+import model.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class EventSystemTest {
+    @BeforeEach
+    void printTestName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    void clearLogs() {
+        Logger.getInstance().clearLog();
+        System.out.println("---"); }
+
+
+
+}
